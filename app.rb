a2b39f83c9976e3edca426017b6a8ef0469c5dee
Cynthia Aguillon <caguillon.techie@gmail.com>
2018-11-27 12:02:30 -0600

    Setting up order form

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ if User.all(administrator: true).count == 0
 	u.save
 end
 
-class Post
+class Order
 	include DataMapper::Resource
 	property :id, Serial
 
@@ -44,4 +44,9 @@ end
 get "/dashboard" do
 	authenticate!
 	erb :dashboard
+end
+
+get "/order" do
+	authenticate!
+	erb :orderform
 end
